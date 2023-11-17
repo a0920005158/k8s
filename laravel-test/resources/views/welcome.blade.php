ae46e76c8edcc6029cbccb42110eb25ce95f10b2
Joan <a0920005158@gmail.com>
2023-11-17 16:33:45 +0800

    xx
    xxx

--- laravel-test/resources/views/welcome.blade.php
+++ laravel-test/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel222x</title>
+        <title>Laravel222xcc</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
